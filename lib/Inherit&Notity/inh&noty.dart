e429d0b77230b538d01f6abf9a5def114a01f7da
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-09-01 08:24:46 +0100

    commit changes

---
+++ lib/Inherit&Notity/inh&noty.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ColorOpacity extends StatelessWidget {
+  const ColorOpacity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Color Opacity'),
+      ),
+      body: Column(
+        children: [
+          Slider(
+            value: 0.0,
+            onChanged: (value) {},
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Container(
+                  height: 150,
+                  color: Colors.green,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 150,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SliderData extends ChangeNotifier {
+  final double _value = 0.0;
+  double get value => _value;
+  set value(double newValue) {
+    if (newValue != _value) {
+      value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
