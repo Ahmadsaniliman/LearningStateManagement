417fe8ab0ec1a74236be7a7039d258a0fbfcfabc
Ahmadsaniliman <saniahmadliman@gmail.com>
2022-12-30 17:34:25 +0100

    InheritedNotifier

--- lib/SliderChange/slider_change.dart
+++ lib/SliderChange/slider_change.dart
@@ -1,1 +1,89 @@
+import 'package:flutter/material.dart';
 
+class SliderData extends ChangeNotifier {
+  double _value = 0.0;
+  double get value => _value;
+  set value(double newValue) {
+    if (newValue != _value) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
+
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier({
+    Key? key,
+    required SliderData sliderData,
+    required Widget child,
+  }) : super(
+          key: key,
+          notifier: sliderData,
+          child: child,
+        );
+
+  static double of(BuildContext context) {
+    return context
+            .dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
+            ?.notifier
+            ?.value ??
+        0.0;
+  }
+}
+
+class SliderHomePage extends StatelessWidget {
+  const SliderHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: SliderInheritedNotifier(
+        sliderData: sliderData,
+        child: Builder(builder: (context) {
+          return Column(
+            children: [
+              Slider(
+                value: SliderInheritedNotifier.of(context),
+                onChanged: (value) {
+                  sliderData.value = value;
+                },
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Opacity(
+                    opacity: SliderInheritedNotifier.of(context),
+                    child: Container(
+                      height: 200.0,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Opacity(
+                    opacity: SliderInheritedNotifier.of(context),
+                    child: Container(
+                      height: 200.0,
+                      color: Colors.green,
+                    ),
+                  ),
+                ].expandEqually().toList(),
+              ),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map(
+        (e) => Expanded(
+          child: e,
+        ),
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:state_mangement_/Inherit/inherit.dart';
+import 'package:state_mangement_/SliderChange/slider_change.dart';
 import 'package:state_mangement_/ValueNot/value_not.dart';
 
 void main() {
@@ -12,14 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'State Management Demo',
-        theme: ThemeData(),
-        home: ApiProvider(
-          api: Api(),
-          child: const InheritedHomePage(),
-        ),
-        routes: {
-          "/new-Contact-Route": (context) => const NewContactPage(),
-        });
+      debugShowCheckedModeBanner: false,
+      title: 'State Management Demo',
+      theme: ThemeData(),
+      home: const SliderHomePage(),
+      routes: {
+        "/new-Contact-Route": (context) => const NewContactPage(),
+      },
+    );
   }
 }
