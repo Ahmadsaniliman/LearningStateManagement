d962b11dcf83a58b25294b2d09b43f0644feb817
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-02-11 22:29:32 +0100

    added provider package

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         ),
         routes: {
           "/new-Contact-Route": (context) => const NewContactView(),
-          "/new-Route": (context) => const NewContactView(),
+          "/new-Route": (context) => const NewBreadCrumbPage(),
         },
       ),
     );
