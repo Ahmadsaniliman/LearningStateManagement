eca39a719ac23acfbb97bdb9c44a7ae0e5daad6a
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-03-31 22:32:05 +0100

    bloc

--- lib/Bloc/bloc.dart
+++ lib/Bloc/bloc.dart
@@ -1,22 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+final persons = [
+  'Muhammad',
+  'Ahmad',
+  'Sani',
+  'Liman',
+];
 
 enum PersonUrl { person1, person2 }
 
-// extension GetRandomName on PersonUrl {
-//     String get getUrl(String url) {
-//         switch(this) {
-//           case PersonUrl.person1:
-//             // TODO: Handle this case.
-//             break;
-//           case PersonUrl.person2:
-//             // TODO: Handle this case.
-//             break;
-//         }
-//     }
-// }
+extension GetRandomName on PersonUrl {
+  String get getUrl {
+    switch (this) {
+      case PersonUrl.person1:
+        return 'http://127.0.0.1:5500/lib/api/person1.json';
+      case PersonUrl.person2:
+        return 'http://127.0.0.1:5500/lib/api/person2.json';
+    }
+  }
+}
+
+extension SubScript<T> on Iterable<T> {
+  T? operator [](int index) => length > index ? elementAt(index) : null;
+}
 
 @immutable
 abstract class LoadAction {
@@ -24,25 +34,116 @@ abstract class LoadAction {
 }
 
 @immutable
-class PersonLoadAction implements LoadAction {}
+class PersonLoadAction implements LoadAction {
+  final PersonUrl url;
+  const PersonLoadAction({
+    required this.url,
+  });
+}
+
+Future<Iterable<Person>> getPersons(String url) => HttpClient()
+    .getUrl(Uri.parse(url))
+    .then((res) => res.close())
+    .then((req) => req.transform(utf8.decoder).join())
+    .then((str) => json.decode(str) as List<dynamic>)
+    .then((list) => list.map((e) => Person.fromJsom(e)));
+
+class Person {
+  final String name;
+  final String age;
+
+  Person.fromJsom(Map<String, String> json)
+      : name = json['name'] as String,
+        age = json['age'] as String;
+}
+
+@immutable
+class FetchedResult {
+  final Iterable<Person> persons;
+  final bool isRetrivedFromCache;
+
+  const FetchedResult({
+    required this.persons,
+    required this.isRetrivedFromCache,
+  });
+
+  @override
+  String toString() =>
+      'FetchedResult (isRetrivedFromCache =  $isRetrivedFromCache, person = $persons)';
+}
+
+class PersonBloc extends Bloc<LoadAction, FetchedResult?> {
+  final Map<PersonUrl, Iterable<Person>> _cache = {};
+  PersonBloc() : super(null) {
+    on<PersonLoadAction>((event, emit) async {
+      final url = event.url;
+      if (_cache.containsKey(url)) {
+        final cachedPerons = _cache[url]!;
+        final result = FetchedResult(
+          persons: cachedPerons,
+          isRetrivedFromCache: true,
+        );
+        emit(result);
+      } else {
+        final persons = await getPersons(url.getUrl);
+        _cache[url] = persons;
+        final result = FetchedResult(
+          persons: persons,
+          isRetrivedFromCache: false,
+        );
+        emit(result);
+      }
+    });
+  }
+}
 
 class BlocHomePage extends StatelessWidget {
   const BlocHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var screenSizeHeight = MediaQuery.of(context).size.height;
-    var screenSizeWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text('AppBar'),
       ),
-      body: Center(
-        child: Container(
-          height: screenSizeHeight * 250,
-          width: screenSizeWidth * 250,
-          color: Colors.green,
-        ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.read<PersonBloc>().add(
+                        const PersonLoadAction(url: PersonUrl.person1),
+                      );
+                },
+                child: const Text('Load Json#1'),
+              ),
+              TextButton(
+                onPressed: () {
+                  context.read<PersonBloc>().add(
+                        const PersonLoadAction(url: PersonUrl.person2),
+                      );
+                },
+                child: const Text('Load Json#2'),
+              ),
+            ],
+          ),
+          BlocBuilder<PersonBloc, FetchedResult?>(
+            buildWhen: (previousResult, currentResult) =>
+                previousResult?.persons != currentResult?.persons,
+            builder: (context, state) => Expanded(
+              child: ListView.builder(
+                itemCount: persons.length,
+                itemBuilder: (context, index) {
+                  final person = persons[index];
+                  return ListTile(
+                    title: Text(person),
+                  );
+                },
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
