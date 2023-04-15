f71c928e5d9dde85e80b0eb698990c66fcf88de5
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-04-15 20:46:09 +0100

    login page with state management(bloc)

--- lib/Bloc/Login/loadiing_screen.dart
+++ lib/Bloc/Login/loadiing_screen.dart
@@ -1,15 +1,87 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 typedef CloseLoadingScreen = bool Function();
 typedef UpdateLoadingScreen = bool Function();
 
 @immutable
-class LoadinScreenController {
+class LoadingScreenController {
   final CloseLoadingScreen close;
   final UpdateLoadingScreen update;
 
-  const LoadinScreenController({
+  const LoadingScreenController({
     required this.close,
     required this.update,
   });
+}
+
+class LoadingScreen {
+  LoadingScreen._sharedInstance();
+  static final _shared = LoadingScreen._sharedInstance();
+  factory LoadingScreen() => _shared;
+
+  LoadingScreenController? _controller;
+
+  LoadingScreenController showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final _text = StreamController<String>();
+    _text.add(text);
+
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) => Material(
+        color: Colors.black.withAlpha(155),
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(
+              maxWidth: size.width * 0.8.toDouble(),
+              maxHeight: size.height * 0.8,
+              minWidth: size.width * 0.5,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 10),
+                    const CircularProgressIndicator(),
+                    const SizedBox(height: 10),
+                    StreamBuilder<String>(
+                      stream: _text.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data!,
+                            textAlign: TextAlign.center,
+                          );
+                        } else {
+                          return Container();
+                        }
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    state.insert(overlay);
+
+    return LoadingScreenController(close: () {
+      return true;
+    }, update: () {
+      return false;
+    });
+  }
 }
