d0a76c1fbbea31c76955ea4d781ffdbd7e6be294
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-05-03 21:56:09 +0100

    work on profile integration

--- lib/Bloc/Login/loadiing_screen.dart
+++ lib/Bloc/Login/loadiing_screen.dart
@@ -85,13 +85,3 @@ class LoadingScreen {
     });
   }
 }
-
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
