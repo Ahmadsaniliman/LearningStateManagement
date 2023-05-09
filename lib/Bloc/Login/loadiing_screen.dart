a6ab38d02b795488113aa7d097181dd394bf1f9b
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-05-09 22:43:41 +0100

    work on ...

--- lib/Bloc/Login/loadiing_screen.dart
+++ lib/Bloc/Login/loadiing_screen.dart
@@ -85,12 +85,3 @@ class LoadingScreen {
     });
   }
 }
-
-class LLLLL extends StatelessWidget {
-  const LLLLL({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
