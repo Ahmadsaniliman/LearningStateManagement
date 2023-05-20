9e7e950438ab8ddbbe2f907d0adbe68a1cee51fc
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-05-20 22:55:44 +0100

    work on ...

--- lib/Bloc/Login/app_action.dart
+++ lib/Bloc/Login/app_action.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/foundation.dart';
 
+@immutable
+class AppAction {}
+
+@immutable
+class LoginAction implements AppAction {
+  final String email;
+  final String password;
+
+  const LoginAction({
+    required this.email,
+    required this.password,
+  });
+}
+
+@immutable
+class LoadNotesAction {
+  const LoadNotesAction();
+}
--- lib/Bloc/Login/login_handle.dart
+++ lib/Bloc/Login/login_handle.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/foundation.dart';
 
+@immutable
+class LoginHandle {
+  final String token;
+
+  const LoginHandle({
+    required this.token,
+  });
+
+  const LoginHandle.fooBar() : token = 'fooBar';
+
+  @override
+  bool operator ==(covariant LoginHandle other) => token == other.token;
+
+  @override
+  int get hashCode => token.hashCode;
+
+  @override
+  String toString() => ' LoginHandle (Token = $token)';
+}
+
+enum LoginErrors { invalidHandle }
