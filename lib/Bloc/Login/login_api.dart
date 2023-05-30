7ba05c21720f08269f0f889dc5f2bebfd34b3e55
Ahmadsaniliman <saniahmadliman@gmail.com>
2023-05-30 22:07:31 +0100

    work on ...

--- lib/Bloc/Login/login_api.dart
+++ lib/Bloc/Login/login_api.dart
@@ -23,12 +23,3 @@ class LoginApi implements LoginApiProtocol {
         (isLoggedIn) => isLoggedIn ? const LoginHandle.fooBar() : null,
       );
 }
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
